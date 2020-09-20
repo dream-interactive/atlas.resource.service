047088d837eaea4bea9de903bea8d0d5ab1e39ec
sevriukovmk <sevriukovmk@gmail.com>
2020-09-20 14:16:11 +0300

    [Discovery] Add eureka client

--- core/src/main/java/core/AtlasApplication.java
+++ core/src/main/java/core/AtlasApplication.java
@@ -3,7 +3,9 @@ package core;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class AtlasApplication{
     public static void main(String[] args) {
